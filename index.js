34288b698098bed285f134acc45b8642a9aadfc0
vexvo <santiago.uribeg@autonoma.edu.co>
2024-09-25 14:34:26 -0500

    commit start repo finished

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import * as dotenv from "dotenv";
 import express from "express";
+dotenv.config();
 
 const app = express();
 
-app.get("/api", (req, res) => {
-	res.send("Hello World");
+// run db config
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+
+// default route
+app.get("/", (req, res) => {
+	res.json("Hello World");
 });
 
-app.listen(5000, () => {
-	console.log("Server is running on port 5000");
+app.listen(process.env.PORT, () => {
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
