031a7a4023823ab91c480ab19705890264143e5b
Jassy0205 <a.z23042016a@gmail.com>
2024-09-25 10:55:44 -0500

    Creación de user... Se crea el modelo, controlador, servicios y ruta para la tabla users y se establece conexión con una base de datos local src, index

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+import routerApi from './src/routes/index.js';
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5001; 
 
 app.get("/api", (req, res) => {
 	res.send("Hello World");
 });
 
-app.listen(5000, () => {
-	console.log("Server is running on port 5000");
+routerApi(app);
+
+app.listen(port, () => {
+	console.log("Server is running on port ", port);
 });
