03c3ac32b7bbe3d54e6f1af0add448fd85b15ad8
vexvo <santiago.uribeg@autonoma.edu.co>
2024-10-09 08:54:31 -0500

    Remove .env file and add to .gitignore

--- src/database/db.js
+++ src/database/db.js
@@ -3,16 +3,29 @@ import config from "../config/config.js";
 import setupModels from "../models/index.js";
 
 const sequelize = new Sequelize(
-	config.dbName, 
-    config.dbUser,
-    config.dbPassword,
-    {
-        host: config.dbHost,
-        dialect: 'postgres'
-    }
+	config.dbName,
+	config.dbUser,
+	config.dbPassword,
+	{
+		host: config.dbHost,
+		dialect: "postgres",
+		operationsAliases: false,
+		dialectOptions: {
+			ssl: {
+				require: true, // Make sure SSL is used
+				rejectUnauthorized: false, // To avoid issues with self-signed certificates
+			},
+		},
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+	}
 );
 
-// sequelize.sync({ force: true });
+// sequelize.sync({ force: false });
 setupModels(sequelize);
 const models = sequelize.models;
 
